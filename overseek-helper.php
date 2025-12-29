d073ac1aeb0b2fb1815c69f4cb96be7a5e493825
MerlinStacks <sam@customkings.com.au>
2025-12-29 13:14:41 +1100

    Fix: Added 'x-store-url' to Access-Control-Allow-Headers in overseek-helper.php to prevent CORS Network Errors.

--- overseek-helper.php
+++ overseek-helper.php
@@ -78,7 +78,7 @@ if (isset($_GET['overseek_direct'])) {
     // Headers
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Authorization, Content-Type');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type, x-store-url, X-WP-Nonce');
     header('Content-Type: application/json');
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') exit;
